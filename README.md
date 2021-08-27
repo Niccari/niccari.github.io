d0b00a5f204b9d3c00a695aa7214efd9e69a5543
Niccari <latieos93@gmail.com>
2021-08-27 22:25:13 +0900

    Add: Creates custom OGP Images for posts

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Portal site for provides web/mobile app works and tech knowledge (powered by [Hugo](https://gohugo.io/)).
 
 Served at https://www.niccari.net/.
+
+## Font LICENSE
+The [Kinto Sans](https://github.com/ookamiinc/kinto) is used on OGP images, and it is provided under SIL Open Font License 1.1. Thanks a lot!
+
+## Author
+niccari
---
+++ bin/make_ogp_image/make_ogp_image.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+root_path="../.."
+content_path="$root_path/content/posts"
+
+if [ ! -d $content_path ]; then
+  echo "content($content_path) not found!"
+  exit 1
+fi
+
+targets=$(find $content_path -name "*.md")
+for target in ${targets[@]}
+do
+  output_dirname="${root_path}/static/assets/img/ogp"
+  output_path="${output_dirname}/$(sed 's|\.md|\.png|g'  <<< $(basename $target))"
+  if [ -f $output_path ]; then
+    echo "$target exists. skip."
+    continue
+  fi
+  tcardgen \
+    --fontDir fonts \
+    --output $output_dirname \
+    --config tcardgen.yaml \
+    $target
+done
+
